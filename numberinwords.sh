e5bf23202c0817f723b2c0630374c081c57926f2
Sharanya Gopal <sharanyagopal11@gmail.com>
2022-06-08 18:47:43 +0530

    digits in numbers

---
+++ numberinwords.sh
@@ -0,0 +1,34 @@
+read -p "Enter digit : " digit
+
+case $digit in
+	1)
+	  echo "one"
+	  ;;
+	2)
+	  echo "two"
+	  ;;
+        3)
+          echo "three"
+          ;;
+        4)
+          echo "four"
+          ;;
+        5)
+          echo "five"
+          ;;
+        6)
+          echo "six"
+          ;;
+        7)
+          echo "seven"
+          ;;
+        8)
+          echo "eight"
+          ;;
+        9)
+          echo "nine"
+          ;;
+        10)
+          echo "ten"
+          ;;
+esac
