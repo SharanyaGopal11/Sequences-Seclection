208aca1fce37dc91d5649b035cc3b102ae8ed1a8
Sharanya Gopal <sharanyagopal11@gmail.com>
2022-06-08 18:51:48 +0530

    days of week

---
+++ weekday.sh
@@ -0,0 +1,25 @@
+read -p "Enter value from 1-7 " day
+
+case $day in
+	1)
+	  echo "sunday"
+	  ;;
+	2)
+	  echo "Monday"
+	  ;;
+	3)
+	  echo "Tuesday"
+	  ;;
+	4)
+	  echo "Wednesday"
+	  ;;
+	5)
+	  echo "Thursday"
+	  ;;
+	6)
+	  echo "Friday"
+	  ;;
+	7)
+	  echo "Saturday"
+	  ;;
+esac
